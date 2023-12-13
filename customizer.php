9584328e67d3bf042621205760ac1141f52dba02
pervez121 <iqbalpervez121@gmail.com>
2023-12-13 19:04:05 +0500

    learned customizer Api

---
+++ customizer.php
@@ -0,0 +1,98 @@
+<!-- this template is for adding options to customizer -->
+
+<?php
+
+function theme_menu_customize_register( $wp_customize ) {
+    // Do stuff with $wp_customize, the WP_Customize_Manager object.
+
+    $wp_customize->add_panel('micropress_menu_setting', array(
+        'title'  => 'Customize Menu Items',
+        'description' => 'This option lets you customize font and color of menu items',
+        'priority' => 10,
+    ));
+
+    $wp_customize->add_section('customize_micropress_menu_font', array(
+        'title'  => 'Menu Font Setting',
+        'panel' => 'micropress_menu_setting', // Corrected 'Panel' to 'panel'
+    ));
+
+    // $wp_customize->add_setting('micropress_menu_font_family', array(
+    //     'type'              => 'theme_mod',
+    //     'capability'        => 'edit_theme_options',
+    //     'default'           => array(
+    //         'family' => 'sans-serif',
+    //         'size'   => '16px',
+    //         'weight' => 'normal',
+    //         'color'  => 'black',
+    //     ),
+    //     'transport'         => 'refresh', // or postMessage
+    //     'sanitize_callback' => 'sanitize_text_field',
+    // ));
+    // Add settingfor each property in their respective sections
+    $wp_customize->add_setting('micropress_menu_font_family', array(
+        'type'              => 'theme_mod',
+        'capability'        => 'edit_theme_options',
+        'default'           => 'sans-serif',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+    
+    $wp_customize->add_setting('micropress_menu_font_size', array(
+        'type'              => 'theme_mod',
+        'capability'        => 'edit_theme_options',
+        'default'           => '16px',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+    
+    $wp_customize->add_setting('micropress_menu_font_weight', array(
+        'type'              => 'theme_mod',
+        'capability'        => 'edit_theme_options',
+        'default'           => 'normal',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+    
+    $wp_customize->add_setting('micropress_menu_font_color', array(
+        'type'              => 'theme_mod',
+        'capability'        => 'edit_theme_options',
+        'default'           => 'black',
+        'transport'         => 'refresh',
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+
+    // Add controls for 'family', 'size', 'weight', 'color'
+    $wp_customize->add_control('micropress_menu_font_family', array(
+        'label'    => __('Font Family'),
+        'section'  => 'customize_micropress_menu_font',
+        'settings' => 'micropress_menu_font_family',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('micropress_menu_font_size', array(
+        'label'    => __('Font Size'),
+        'section'  => 'customize_micropress_menu_font',
+        'settings' => 'micropress_menu_font_size',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('micropress_menu_font_weight', array(
+        'label'    => __('Font Weight'),
+        'section'  => 'customize_micropress_menu_font',
+        'settings' => 'micropress_menu_font_weight',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('micropress_menu_font_color', array(
+        'label'    => __('Font Color'),
+        'section'  => 'customize_micropress_menu_font',
+        'settings' => 'micropress_menu_font_color',
+        'type'     => 'color',
+    ));
+}
+add_action( 'customize_register', 'theme_menu_customize_register' );
+
+
+
+
+?>
--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
+require 'customizer.php';
 function micropress_setup() {
 	/*
 		* Make theme available for translation.
